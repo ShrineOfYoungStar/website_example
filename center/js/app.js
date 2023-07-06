4054e57959de1016bbf97026f65940d95ecde53e
ShrineOfYoungStar <138499415+ShrineOfYoungStar@users.noreply.github.com>
2023-07-06 15:20:57 +0500

    Add files via upload

---
+++ center/js/app.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', e => {
+    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+})
+
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+}
+
